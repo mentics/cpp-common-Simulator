323ee5accceaffe314718572729075ed38eb4b19
Laurent Zubiaur <zubiaur.laurent@gmail.com>
2017-11-22 17:04:59 +0100

    Add resettable data-structure

---
+++ include/Stateful.h
@@ -0,0 +1,90 @@
+#include <boost/circular_buffer.hpp>
+#include "MenticsCommon.h"
+
+namespace MenticsGame {
+
+	template<typename T> using Action = std::function<void(T&)>;
+
+	template<typename T,typename TimeType>
+	struct Change {
+		Change(Action<T> action, TimeType const& time) : action(action), time(time) {}
+		TimeType time;
+		Action<T> action;
+	};
+
+	template<typename T, typename TimeType> using ChangeCallback = std::function<bool (Change<T,TimeType> const&)>;
+
+	class StatefulTest;
+
+	template<typename T, typename TimeType>
+	class Stateful
+	{
+	public:
+		Stateful(T t, int capacity) : stateOldest(t), stateCurrent(t), buffer(capacity) {}
+		Stateful(T t, int capacity, TimeType time) : stateOldest(t), stateCurrent(t), buffer(capacity), timeCurrent(time) {}
+		~Stateful() {}
+		T getCurrentState() { return stateCurrent; }
+		void apply(Change<T,TimeType> const& change);
+		void moveOldest(TimeType const& time);
+		void reset(TimeType const& time);
+		void walk(ChangeCallback<T,TimeType> const& callback);
+		friend StatefulTest;
+	protected:
+		boost::circular_buffer<Change<T,TimeType>> buffer;
+		TimeType timeCurrent;
+		T stateOldest, stateCurrent;
+	};
+
+	template<typename T,typename TimeType>
+	void Stateful<T, TimeType>::apply(Change<T, TimeType> const& change)
+	{
+		assert(change.time > timeCurrent);
+		if (buffer.full()) {
+			buffer.back().action(stateOldest);
+		}
+		change.action(stateCurrent);
+		buffer.push_back(change);
+		timeCurrent = change.time;
+	}
+
+	template<typename T, typename TimeType>
+	void Stateful<T, TimeType>::moveOldest(TimeType const& time)
+	{
+		if (time > timeCurrent) {
+			return;
+		}
+		auto first = std::find_if(buffer.begin(), buffer.end(), [&](Change<T, TimeType> const& c) {
+			if (c.time <= time) {
+				c.action(stateOldest);
+				return false;
+			}
+			return true;
+		});
+		buffer.erase(buffer.begin(), first);
+	}
+
+	template<typename T, typename TimeType>
+	void Stateful<T, TimeType>::walk(ChangeCallback<T,TimeType> const& callback)
+	{
+		std::find_if(buffer.begin(), buffer.end(), callback);
+	}
+
+	template<typename T, typename TimeType>
+	void Stateful<T, TimeType>::reset(TimeType const& time)
+	{
+		if (time > timeCurrent || time < buffer.front().time) {
+			return;
+		}
+		stateCurrent = stateOldest;
+		walk([&](Change<T, TimeType> const &change)->bool {
+			if (change.time <= time) {
+				change.action(stateCurrent);
+				timeCurrent = change.time;
+				return false;
+			}
+			return true;
+		});
+		stateOldest = stateCurrent;
+		buffer.clear();
+	}
+}
---
+++ test/StatefulTest.cpp
@@ -0,0 +1,172 @@
+#include "stdafx.h"
+#include "Stateful.h"
+#include "CppUnitTest.h"
+#include "MenticsCommonTest.h"
+//#include "date.h"
+#include <iostream>
+#include <string>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+//std::string timeToString(std::chrono::system_clock::time_point const& tp) {
+//	using namespace date;
+//	std::stringstream buffer;
+//	buffer << tp;
+//	return buffer.str();
+//}
+
+namespace MenticsGame {
+
+	using Time = std::chrono::system_clock::time_point;
+
+	struct Entity {
+		Entity() : value(0) {}
+		int value;
+	};
+	bool operator==(Entity const& a, Entity const &b) {
+		return a.value == b.value;
+	}
+
+	TEST_CLASS(StatefulTest)
+	{
+		boost::log::sources::severity_logger<boost::log::trivial::severity_level> lg;
+		const std::string name = "StatefulTest";
+
+	public:
+		TEST_CLASS_INITIALIZE(BeforeClass) {
+			setupLog();
+		}
+
+		TEST_METHOD(TestStateBufferFull) {
+			Entity ent;
+			Stateful<Entity, Time> state(ent,2);
+			Assert::AreEqual(0, state.stateCurrent.value);
+			Assert::AreEqual(state.stateCurrent.value, state.stateOldest.value);
+
+			auto inc = [](Entity &ent) {
+				ent.value += 1;
+			};
+
+			state.apply(Change<Entity, Time>(inc, std::chrono::system_clock::now()));
+			Assert::AreEqual(1, (int)state.buffer.size());
+			Assert::AreEqual(0, state.stateOldest.value);
+			Assert::AreEqual(1, state.stateCurrent.value);
+
+			state.apply(Change<Entity, Time>(inc, std::chrono::system_clock::now()));
+			state.apply(Change<Entity, Time>(inc, std::chrono::system_clock::now()));
+			Assert::AreEqual(3, state.stateCurrent.value);
+			Assert::IsTrue(state.buffer.full());
+			Assert::AreEqual(1, state.stateOldest.value);
+
+			//state.apply(Change<Entity, Time>(setToOne, Time())); // Assertion fails
+		}
+
+		TEST_METHOD(TestStateMoveOldest) {
+			Entity ent;
+			Stateful<Entity, Time> state(ent, 10);
+
+			auto inc = [](Entity &ent) {
+				ent.value += 1;
+			};
+
+			std::vector<Time> times;
+			for (int i = 0; i < 10; ++i) {
+				Time t = std::chrono::system_clock::now();
+				times.push_back(t);
+				state.apply(Change<Entity, Time>(inc, t));
+			}
+			Assert::AreEqual(10, state.stateCurrent.value);
+			Assert::AreEqual(0, state.stateOldest.value);
+
+			//state.walk([&](Change<Entity, Time> const& change) -> bool {
+			//	LOG(lvl::info) << timeToString(change.time) << " " << timeToString(times.at(4)) << " " << (change.time <= times.at(4));
+			//	return true;
+			//});
+
+			state.moveOldest(times.at(4));
+			Assert::AreEqual(5, state.stateOldest.value);
+			Assert::AreEqual(5, (int)state.buffer.size());
+
+			Entity tmp = state.stateOldest;
+			state.walk([&tmp](Change<Entity, Time> const& change) -> bool {
+				change.action(tmp);
+				return false;
+			});
+			Assert::AreEqual(tmp.value, state.stateCurrent.value);
+
+			// Moving to time > current time leaves the buffer unchanged
+			state.moveOldest(std::chrono::system_clock::now());
+			Assert::AreEqual(5, (int)state.buffer.size(),L"Buffer changed");
+
+			state.moveOldest(times.back());
+			LOG(lvl::info) << state.buffer.size();
+			Assert::IsTrue(state.buffer.empty(),L"Buffer is not empty");
+			Assert::AreEqual(state.stateCurrent.value, state.stateOldest.value);
+		}
+
+		typedef uint64_t RealTime;
+		
+		TEST_METHOD(TestStateCommand) {
+			class Command {
+			public:
+				Command(Stateful<Entity, RealTime> *target) : target(target) {}
+				void execute() {
+					RealTime now = std::chrono::system_clock::now().time_since_epoch().count();
+					Change<Entity, RealTime> change(action, now);
+					target->apply(change);
+				}
+			protected:
+				Action<Entity> action;
+				Stateful<Entity, RealTime> *target;
+			};
+
+			class IncrementCommand : public Command {
+			public:
+				IncrementCommand(Stateful<Entity, RealTime> *target) : Command(target) {
+					action = [](Entity &e) {
+						e.value++;
+					};
+				}
+			};
+
+			Entity ent;
+			ent.value = 1;
+			Stateful<Entity, RealTime> state(ent, 10, 0);
+
+			IncrementCommand inc(&state);
+			inc.execute();
+			inc.execute();
+			Assert::AreEqual(1, state.stateOldest.value);
+			Assert::AreEqual(3, state.stateCurrent.value);
+		}
+
+		TEST_METHOD(TestStateReset) {
+			Entity ent;
+			ent.value = 1;
+			Stateful<Entity, Time> state(ent, 10);
+
+			auto before = std::chrono::system_clock::now();
+
+			std::vector<Time> times;
+			for (int i = 0; i < 10; ++i) {
+				Time now = std::chrono::system_clock::now();
+				times.push_back(now);
+				state.apply(Change<Entity, Time>([](Entity &e) {
+					e.value = e.value * 2;
+				},now));
+			}
+
+			state.reset(before);
+			Assert::AreEqual(10, (int)state.buffer.size(), L"Cannot be reset to older than the oldest state");
+
+			state.reset(std::chrono::system_clock::now());
+			Assert::AreEqual(10, (int)state.buffer.size(), L"Cannot be reset to newer than the current state");
+			
+			state.reset(times.at(4));
+			Assert::IsTrue(state.buffer.empty());
+			Assert::AreEqual(32, state.stateCurrent.value);
+			Assert::IsTrue(state.stateCurrent == state.stateOldest);
+			Assert::IsTrue(state.timeCurrent == times.at(4));
+		}
+	};
+}
