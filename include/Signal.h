ef661e3620e932e7ac4f47b417b294c0d19103a1
Antonio Trpeski <antuan.trpeski@hotmail.gr>
2018-03-01 23:38:14 +0200

    added TeamId typedef, fixed commented fields in Agent

--- include/Signal.h
+++ include/Signal.h
@@ -10,22 +10,22 @@ namespace MenticsGame
 	{
 	protected:
 		struct ValueAtTime { T value; TimeType at; };
-		std::deque<ValueAtTime> Values;
+		std::deque<ValueAtTime> values;
 	public:
 		void reset(TimeType resetTime)
 		{
-			while (resetTime < Values.back())Values.pop_back();
+			while (resetTime < values.back())values.pop_back();
 		}
 
 		void add(T val, TimeType t)
 		{
-			Values.push_back(ValueAtTime{ val , t });
+			values.push_back(ValueAtTime{ val , t });
 		}
 
 		T get(TimeType at)
 		{
-			for (std::deque<ValueAtTime>::reverse_iterator i = Values.rbegin();
-				i != Values.rend(); ++i)
+			for (std::deque<ValueAtTime>::reverse_iterator i = values.rbegin();
+				i != values.rend(); ++i)
 			{
 				if (i->at <= at){
 					return i->value;
@@ -35,8 +35,8 @@ namespace MenticsGame
 
 		void removeOldest(TimeType upTo)
 		{
-			for (auto time = Values.front().at; time < upTo; time = Values.front().at) {
-				Values.pop_front();
+			for (auto time = values.front().at; time < upTo; time = values.front().at) {
+				values.pop_front();
 			}
 		}
 
